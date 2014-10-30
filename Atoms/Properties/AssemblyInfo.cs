e7b5bb41a590a6a648c499945f32bddd76942a61
Pavel Mayorov <mayorovp@sonda-lab.ru>
2014-10-30 21:54:49 +0500

    Solution created

---
+++ Atoms/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Atoms")]
+[assembly: AssemblyCopyright("MIT License")]
+[assembly: AssemblyDescription("https://github.com/mayorovp/atoms")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
